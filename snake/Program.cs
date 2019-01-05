61f35ec828b8c9e2c6fdb4e5227a20098ebc26bf
Kkidy <kkidywork@gmail.com>
2019-01-06 02:32:31 +0500

    Заменил первую точку на класс Point

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,11 +10,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sum1 = '*';
 
-            Draw(x1, y1, sum1);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sum = '*';
+            Draw(p1.x, p1.y, p1.sum);
+
+            /*  int x1 = 1;
+            int y1 = 3;
+            char sum1 = '*'; 
+
+            Draw(x1, y1, sum1); */
 
             int x2 = 4;
             int y2 = 5;
