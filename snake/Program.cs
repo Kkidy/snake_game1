77fc3cd5f29cf7b0488fa3d45dfb4c73f9337bc9
Kkidy <kkidywork@gmail.com>
2019-01-06 02:02:50 +0500

    Добавил вывод символа на экран

--- snake/Program.cs
+++ snake/Program.cs
@@ -13,6 +13,12 @@ namespace Snake
             int x1 = 1;
             int y1 = 3;
             char sum1 = '*';
+
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sum1);
+
+
+
             Console.ReadLine();
         }
     }
