e1c8893d22cd882e3cf83bcd51fa095c45becf81
Kkidy <kkidywork@gmail.com>
2019-01-06 02:44:14 +0500

    Добавил функцию Draw в класс Point

--- snake/Program.cs
+++ snake/Program.cs
@@ -15,13 +15,13 @@ namespace Snake
             p1.x = 1;
             p1.y = 3;
             p1.sum = '*';
-            Draw(p1.x, p1.y, p1.sum);
+            p1.Draw();
 
             Point p2 = new Point();
             p2.x = 4;
             p2.y = 5;
             p2.sum = '#';
-            Draw(p2.x, p2.y, p2.sum);
+            p2.Draw();
 
 
 
