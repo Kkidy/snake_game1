dc187fe46bf5fbe0d5f2779ac79a20f3cccd4a1e
Kkidy <kkidywork@gmail.com>
2019-01-06 23:16:14 +0500

    Добавлен конструктор в класс Point

--- snake/Snake/Point.cs
+++ snake/Snake/Point.cs
@@ -10,13 +10,19 @@ namespace Snake
     {
         public int x;
         public int y;
-        public char sum;
-       
+        public char sym;
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
 
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
-            Console.Write(sum);
+            Console.Write(sym);
         }
     }
 }
